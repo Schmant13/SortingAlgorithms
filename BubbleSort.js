bdaad6abd53d7313c6bbf4221c0c251aa04b11a2
Anton Mühlbach <antonmuehlbach@icloud.com>
2023-11-24 23:31:24 +0100

    UnsortedList input (not working)

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,4 +1,11 @@
-function getUnsortedList(){
-    var unsortedList = document.getElementById("frm1").onsubmit;
-    console.log(unsortedList);
-}
+document.getElementById("frm1").addEventListener("submit", function(e){
+    let inputs = document.getElementsByClassName("text");
+    console.log("inputs:"+inputs);
+    var unsortedList = [];
+    for (let i = 0; i<inputs.length; i++){
+        unsortedList.push(inputs[i].value);
+        console.log(unsortedList[unsortedList.length - 1]);
+        }
+    }
+)
+
